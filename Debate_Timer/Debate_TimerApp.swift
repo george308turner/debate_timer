5f4ff93489a610fb21e9bf3a4001770cb3340d49
george308turner <georgeturner308@icloud.com>
2025-01-28 22:06:02 +0000

    Initial Commit

---
+++ Debate_Timer/Debate_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Debate_TimerApp.swift
+//  Debate_Timer
+//
+//  Created by George Turner on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Debate_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Debate_TimerTests/Debate_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Debate_TimerTests.swift
+//  Debate_TimerTests
+//
+//  Created by George Turner on 1/28/25.
+//
+
+import Testing
+@testable import Debate_Timer
+
+struct Debate_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
